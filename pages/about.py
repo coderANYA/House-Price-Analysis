350e4b575ed4127b5f0e87ba0ab5acbe09ab8549
ananya <aananya30121998@gmail.com>
2023-07-08 12:22:49 +0530

    first commit

---
+++ pages/about.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+#create a title for your app
+st.title('House Price Data Analysis')
+st.subheader('About this App')
+
+st.markdown("""
+This app performs simple data analysis on the **House Price** dataset.
+* **Python libraries: **
+    - pandas
+    - streamlit
+    - numpy
+    - matplotlib
+    - seaborn
+* ** Data Source:** [Kaggle](https://www.kaggle.com/harlfoxem/housesalesprediction)
+""")
---
+++ readme.md
@@ -0,0 +1,10 @@
+1. Run on local machine
+``````
+pip install - r requirements.txt
+``````
+
+2. After installing all the dependencies, run the following command in the terminal 
+
+``````
+streamlit run app.py
+``````
